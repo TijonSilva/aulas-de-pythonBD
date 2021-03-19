3cf99fe96acf7d2ce17a537ed3d9efcf3ee5b775
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-03-19 08:28:29 -0300

    save

--- 1aula.py
+++ 1aula.py
@@ -7,12 +7,5 @@ for test in p:
   print('***')
 print('fim')
 import csv
-
-with open('.csv') as csv_file:
-    
-    csv_reader = csv.reader(csv_file, delimiter=',')
-
-    csv_reader.__next__()
-
-    for row in csv_reader:
-        print( row[0] + ', ' + row[1] + ', ' + row[2] )
+teste = pd.read_csv('plandados.csv', delimiter= ';', decimal= ',')
+print (teste)
